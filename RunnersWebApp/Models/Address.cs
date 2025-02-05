07aacf5605dd40e04f63527da7c1b0716dafecbb
kanix21 <kk782@student.aru.ac.uk>
2025-02-05 15:23:07 +0800

    small tweaks

--- RunnersWebApp/Models/Address.cs
+++ RunnersWebApp/Models/Address.cs
@@ -5,8 +5,8 @@ namespace RunnersWebApp.Models
     {
         [Key]
         public int Id { get; set; }
-        public string Street { get; set; }
+        public string Country { get; set; }
         public string City { get; set; }
-        public string State { get; set; }
+        public string Street { get; set; }
     }
 }
