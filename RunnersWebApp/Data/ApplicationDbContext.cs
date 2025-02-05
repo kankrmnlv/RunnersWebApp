b0e9ca356a085585459477225303fc3cc73c29be
kanix21 <kk782@student.aru.ac.uk>
2025-02-05 20:37:54 +0800

    started adding identity

--- RunnersWebApp/Data/ApplicationDbContext.cs
+++ RunnersWebApp/Data/ApplicationDbContext.cs
@@ -1,9 +1,10 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using RunnersWebApp.Models;
 
 namespace RunnersWebApp.Data
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : IdentityDbContext<AppUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
--- RunnersWebApp/Models/AppUser.cs
+++ RunnersWebApp/Models/AppUser.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace RunnersWebApp.Models
 {
@@ -7,6 +8,8 @@ namespace RunnersWebApp.Models
         public int? Pace { get; set; }
 
         public int? Mileage { get; set; }
+        [ForeignKey("Address")]
+        public int AddressId { get; set; }
         public Address? Address { get; set; }
 
         public ICollection<Club> Clubs { get; set; }
