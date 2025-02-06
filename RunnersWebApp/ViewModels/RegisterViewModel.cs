b036e35b42158d7dc160cccaf87e0f7527054325
kanix21 <kk782@student.aru.ac.uk>
2025-02-06 15:36:29 +0800

    working on register page

---
+++ RunnersWebApp/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RunnersWebApp.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Display(Name = "Email address")]
+        [Required(ErrorMessage = "Email address is required")]
+        public string EmailAddress { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Display(Name = "Confirm password")]
+        [Required(ErrorMessage = "Please confirm your password")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Passwords do not match")]
+        public string ConfirmPassword { get; set; }
+    }
+}
