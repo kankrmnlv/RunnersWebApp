1a35f6ab5b669d6705386e9a8bbd631362cf03da
kanix21 <kk782@student.aru.ac.uk>
2025-02-07 18:14:38 +0800

    ip geo location is now detected, and club in the area are shown.

--- RunnersWebApp/Controllers/HomeController.cs
+++ RunnersWebApp/Controllers/HomeController.cs
@@ -1,21 +1,54 @@
 using System.Diagnostics;
+using System.Globalization;
+using System.Net;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RunnersWebApp.Helpers;
+using RunnersWebApp.Interfaces;
 using RunnersWebApp.Models;
+using RunnersWebApp.ViewModels;
 
 namespace RunnersWebApp.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IClubInterface _clubInterface;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IClubInterface clubInterface)
         {
             _logger = logger;
+            _clubInterface = clubInterface;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var ipInfo = new IPInfo();
+            var homeViewModel = new HomeViewModel();
+            try
+            {
+                string url = "https://ipinfo.io?token=dc4f9543fbc402";
+                var info = new WebClient().DownloadString(url);
+                ipInfo = JsonConvert.DeserializeObject<IPInfo>(info);
+                RegionInfo myRegion = new RegionInfo(ipInfo.Country);
+                ipInfo.Country = myRegion.EnglishName;
+                homeViewModel.City = ipInfo.City;
+                homeViewModel.Country = ipInfo.Country;
+                if(homeViewModel.City != null)
+                {
+                    homeViewModel.Clubs = await _clubInterface.GetClubByCity(homeViewModel.City);
+                }
+                else
+                {
+                    homeViewModel.Clubs = null;
+                }
+                return View(homeViewModel);
+            }
+            catch (Exception ex)
+            {
+                homeViewModel.Clubs = null;
+            }
+            return View(homeViewModel);
         }
 
         public IActionResult Privacy()
---
+++ RunnersWebApp/ViewModels/HomeViewModel.cs
@@ -0,0 +1,12 @@
+﻿using RunnersWebApp.Models;
+
+namespace RunnersWebApp.ViewModels
+{
+    public class HomeViewModel
+    {
+        public IEnumerable<Club> Clubs { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
+
+    }
+}
