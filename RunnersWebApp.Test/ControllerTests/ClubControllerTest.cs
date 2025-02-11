80f70457e8c6fbd02648f11f1bdbf767f2fa4c4d
kanix21 <kk782@student.aru.ac.uk>
2025-02-11 17:58:54 +0800

    started unit testing, tested the clubcontroller

---
+++ RunnersWebApp.Test/ControllerTests/ClubControllerTest.cs
@@ -0,0 +1,62 @@
+﻿using FakeItEasy;
+using FluentAssertions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RunnersWebApp.Controllers;
+using RunnersWebApp.Interfaces;
+using RunnersWebApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RunnersWebApp.Test.ControllerTests
+{
+    public class ClubControllerTest
+    {
+        private ClubController _clubController;
+        private IClubInterface _clubInterface;
+        private IPhotoInterface _photoInterface;
+        private IHttpContextAccessor _contextAccessor;
+
+        public ClubControllerTest()
+        {
+            _clubInterface = A.Fake<IClubInterface>();
+            _photoInterface = A.Fake<IPhotoInterface>();
+            _contextAccessor = A.Fake<HttpContextAccessor>();
+
+            //SUT
+            _clubController = new ClubController(_clubInterface, _photoInterface, _contextAccessor);
+        }
+
+        [Fact]
+        public void ClubController_Index_ReturnsSuccess()
+        {
+            //Arrange
+            var clubs = A.Fake<IEnumerable<Club>>();
+            A.CallTo(() => _clubInterface.GetAll()).Returns(clubs);
+
+            //Act
+            var result = _clubController.Index();
+
+            //Assert
+            result.Should().BeOfType<Task<IActionResult>>();
+        }
+        [Fact]
+        public void ClubController_Detail_ReturnsSuccess()
+        {
+            //Arrange
+            var id = 1;
+            var club = A.Fake<Club>();
+            A.CallTo(() => _clubInterface.GetByIdAsync(id)).Returns(club);
+
+            //Act
+            var result = _clubController.Detail(id);
+
+            //Assert
+            result.Should().BeOfType<Task<IActionResult>>();
+
+        }
+    }
+}
---
+++ RunnersWebApp.Test/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace RunnersWebApp.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
